735ff19ac89419963b19450a53e842e7146811c8
Smitkumar Patel <30848598+smitkumarpatel4@users.noreply.github.com>
2018-11-15 12:50:38 -0800

    Implementing Username Fetching and authentification security

--- src/main/java/com/munachimsoani/android/personalfeedapp/ChatActivity.java
+++ src/main/java/com/munachimsoani/android/personalfeedapp/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.munachimsoani.android.personalfeedapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,12 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,6 +32,7 @@ import com.munachimsoani.android.personalfeedapp.model.Message;
 import com.munachimsoani.android.personalfeedapp.utilities.MessageAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
@@ -33,11 +40,14 @@ public class ChatActivity extends AppCompatActivity {
     private static final String TAG = "ChatActivity";
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    public static final int RC_SIGN_IN = 1;
+
 
     private ListView mMessageListView;
     private MessageAdapter mAdapter;
 
     private ProgressBar mProgressBar;
+
     private ImageButton mPhotoPickerButton;
     private EditText mMessageEditText;
     private Button mSendButton;
@@ -45,9 +55,12 @@ public class ChatActivity extends AppCompatActivity {
     private String mUsername;
 
     //firebase database varieable
+    private FirebaseUser mUser;
     private FirebaseDatabase mFbDatabase;
     private DatabaseReference mMessageDbReference;
     private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFbAuth;
+    private FirebaseAuth.AuthStateListener mAuthSateListner;
 
 
     @Override
@@ -58,7 +71,6 @@ public class ChatActivity extends AppCompatActivity {
 
         mUsername = ANONYMOUS;
 
-        // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
         mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
@@ -73,17 +85,17 @@ public class ChatActivity extends AppCompatActivity {
         // Initialize progress bar
         mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
-        //Initializing Database Reference
+        //Initializing Firebase Function
         mFbDatabase = FirebaseDatabase.getInstance();
+        mFbAuth = FirebaseAuth.getInstance();
+
         mMessageDbReference = mFbDatabase.getReference().child("messages");
-
-
 
         // ImagePickerButton shows an image picker to upload a image for a message
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                //for image file code setup
             }
         });
 
@@ -115,7 +127,6 @@ public class ChatActivity extends AppCompatActivity {
                 // Send messages on click
                 //get a message from Message Edit text
                 Message message = new Message(mMessageEditText.getText().toString() , mUsername, null);
-
                 // send a message to the database reference
                 mMessageDbReference.push().setValue(message);
                 // Clear input box
@@ -123,51 +134,117 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
-
-        mChildEventListener = new ChildEventListener() {
+        //Authentication Listner
+        mAuthSateListner = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Message message = dataSnapshot.getValue(Message.class);
-                mAdapter.add(message);
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // Check the whether user is sign in or not
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if ( user != null){
+                    // when user is signed in
+                    onSignedInInitialize(user.getDisplayName());
+                }
+//                else  // when use is not loged in
+//                { }
             }
         };
-
-        mMessageDbReference.addChildEventListener(mChildEventListener);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.chat_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+//        click on sign out
+        switch (item.getItemId()){
+            case R.id.chat_out_menu:
+                Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+       return super.onOptionsItemSelected(item);
     }
 
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if(requestCode == RC_SIGN_IN){
+//            if (requestCode ==RESULT_OK){
+//                Toast.makeText(this,"Signed in!",Toast.LENGTH_LONG).show();
+//            }
+//            else if (requestCode == RESULT_CANCELED){
+//                Toast.makeText(this,"Signed in Canceled!",Toast.LENGTH_LONG).show();
+//                finish();
+//            }
+//        }
+//    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFbAuth.addAuthStateListener(mAuthSateListner);
+        detachDatabaseReadListner();
+        mAdapter.clear();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFbAuth.removeAuthStateListener(mAuthSateListner);
+    }
+
+    private void onSignedInInitialize(String username){
+        mUsername = username;
+        attachDatabaseReadListner();
+    }
+    private void onSignedOutInitialize(){
+        mUsername = ANONYMOUS;
+        mAdapter.clear();
+        detachDatabaseReadListner();
+    }
+    private void attachDatabaseReadListner(){
+        //Child event Listner for geting message
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Message message = dataSnapshot.getValue(Message.class);
+                    mAdapter.add(message);
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                }
+            };
+        }
+
+        mMessageDbReference.addChildEventListener(mChildEventListener);
+
+    }
+
+    private void detachDatabaseReadListner(){
+
+        if (mChildEventListener !=  null) {
+            mMessageDbReference.removeEventListener(mChildEventListener);
+        }
+    }
 }
